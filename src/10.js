69bf2da855eb45f9423e648ca2d087b4b8bdefd6
hurbanriggles80 <hurbanriggles80@gmail.com>
2025-03-16 17:52:04 +0000

    Add 10.js

---
+++ src/10.js
@@ -0,0 +1,7 @@
+// Function to generate a random number between 1 and 10
+function getRandomNumber() {
+  return Math.floor(Math.random() * 10) + 1;
+}
+
+// Get a random number and print it to the console
+console.log(getRandomNumber());
