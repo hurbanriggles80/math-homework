8af4d839c32bb49d0d37581da37867fe67fd01fd
hurbanriggles80 <hurbanriggles80@gmail.com>
2025-05-26 09:46:17 +0000

    Add 40.js

---
+++ src/40.js
@@ -0,0 +1,15 @@
+function fibonacci(n) {
+    if (n <= 1) {
+        return n;
+    } else {
+        let a = 0, b = 1;
+        for (let i = 2; i < n; i++) {
+            let c = a + b;
+            a = b;
+            b = c;
+        }
+        return b;
+    }
+}
+
+console.log(fibonacci(10)); // Output: 55
